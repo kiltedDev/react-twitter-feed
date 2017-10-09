2d1efcd955d421cce55555d0899e20b57a0dd52d
Thomas Wilson <twilson.spqr@gmail.com>
2017-10-09 19:33:21 -0400

    tweets tweeted

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,2 +1,93 @@
-const Tweet = () => null;
+import React from 'react';
+
+const Tweet = props => {
+  let date = new Date(+props.timestamp).toDateString().split(' ');
+
+  let favoriteClassName;
+
+  if (props.favorited) {
+    favoriteClassName = "red-text";
+  } else {
+    favoriteClassName = "";
+  }
+
+  let retweetClassName;
+
+  if (props.retweeted) {
+    retweetClassName = "green-text";
+  } else {
+    retweetClassName = "";
+  }
+
+  let imgDiv;
+
+  if (props.tweetImgUrl){
+    imgDiv = <img src= {props.tweetImgUrl}/>
+  }
+
+  return(
+    <div className = "row box">
+      <div className = "small-8 small-centered columns tweetbox">
+        <div className = "row allContent">
+          <div className = "small-2 columns">
+            <img src= {props.profileImgUrl}/>
+          </div>
+          <div className = "small-10 columns">
+
+            <div className = "row">
+              <div className = "small-10 end columns">
+                <ul>
+                  <li>
+                    {props.userName}
+                  </li>
+                  <li className = "grey-text">
+                    @{props.screenName}
+                  </li>
+                  <li className = "grey-text">
+                    {props.timestamp}
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className ="row">
+              <div className = "small-12 columns">
+                <p className = "tweettext"> {props.text}</p>
+
+              </div>
+              <p className = "imagePicture">
+                {imgDiv}
+              </p>
+            </div>
+
+            <div className = "row">
+              <div className = "small-8 columns">
+                <ul>
+                  <li>
+                    <i className="fa fa-reply" aria-hidden="true" onClick={props.replyAlert}></i>
+                  </li>
+                  <li>
+                    <div className={retweetClassName}>
+                      <i className="fa fa-retweet" aria-hidden="true" onClick={props.retweetAlert}> {props.retweetCount}</i>
+                    </div>
+                  </li>
+                  <li>
+                    <div className={favoriteClassName}>
+                      <i className="fa fa-heart" aria-hidden="true" onClick={props.likeAlert}> {props.favoriteCount}</i>
+                    </div>
+                  </li>
+                  <li>
+                    <i className="fa fa-ellipsis-h" aria-hidden="true" onClick={props.moreAlert}></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Tweet;
--- src/components/TwitterFeed.js
+++ src/components/TwitterFeed.js
@@ -1,3 +1,56 @@
 import React from 'react';
-const TwitterFeed = () => <div />;
+import Tweet from '../components/Tweet';
+
+const TwitterFeed = (props) => {
+  let twitterFeed = props.data.map((tweet) => {
+    let replyAlert = () => alert("reply");
+
+    let retweetAlert = () => alert("retweet");
+
+    let likeAlert = () => alert("like");
+
+    let moreAlert = () => alert("favorite");
+
+    let tweetImg;
+    let tweetImageDisplay;
+    let tweetText = tweet.text;
+
+    if (Object.getOwnPropertyNames(tweet.entities).length > 0){
+      tweetImg = tweet.entities.media[0].media_url;
+      tweetImageDisplay = tweet.entities.media[0].media_url;
+      tweetText = tweetText.replace(` ${tweetImageDisplay}`,'');
+    }
+
+
+    let date = new Date(Number(tweet.timestamp_ms)).toString().split(" ")
+    date = `${date[1]} ${date[2]}`
+
+    return (
+      <Tweet
+      key = {tweet.id_str}
+      text = {tweet.text}
+      userName = {tweet.user.name}
+      screenName = {tweet.user.screen_name}
+      profileImgUrl = {tweet.user.profile_image_url}
+      retweetCount = {tweet.retweet_count}
+      favoriteCount = {tweet.favorite_count}
+      favorited = {tweet.favorited}
+      retweeted = {tweet.retweeted}
+      timestamp = {date}
+      tweetImgUrl = {tweetImg}
+      tweetImgText = {tweetImageDisplay}
+      replyAlert = {replyAlert}
+      retweetAlert = {retweetAlert}
+      likeAlert = {likeAlert}
+      moreAlert = {moreAlert}
+      />
+    )})
+
+  return (
+    <div id="twitter-feed">
+      {twitterFeed}
+    </div>
+  )
+}
+
 export default TwitterFeed;
